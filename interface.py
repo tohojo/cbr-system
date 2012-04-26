7d27b511b28523e71963e2a6d9ca55995d1695f0
Toke Høiland-Jørgensen <thoe898@aucklanduni.ac.nz>
2012-04-26 17:28:18 +1200

    Add better help, status function, cases to interface.

--- interface.py
+++ interface.py
@@ -19,14 +19,34 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import re
+
 from console import Console
 
 class Interface(Console):
 
-    def __init__(self):
+    def __init__(self, cases=[]):
         Console.__init__(self)
         self.prompt = ">> "
         self.intro = "Welcome to the CBR system. Type 'help' for a list of commands."
+        self.cases = cases
+        if not cases:
+            self.intro += "\nNOTE: Currently no cases loaded (you may want to run parser.py to generate some)!"
+
+    def gen_help(self, method):
+        """Generate a help message by removing extra spaces from doc strings"""
+        helpstring = getattr(self.__class__, method).__doc__
+        return re.sub("\n *", "\n", helpstring)
+
+    def help_help(self):
+        print self.gen_help("do_help"),
+
+    def do_status(self, arg):
+        """Print current status of system (i.e. how many cases loaded etc)."""
+        print "Current %d cases loaded." % len(self.cases)
+
+    def help_status(self):
+        print self.gen_help("do_status")
 
     def default(self, line):
         print "Invalid command. Type 'help' for a list of commands."
