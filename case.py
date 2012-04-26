ed7e45dee3979c3a6998f20a183d2b998d735083
Toke Høiland-Jørgensen <thoe898@aucklanduni.ac.nz>
2012-04-26 17:07:46 +1200

    Add file headers to all files.

--- case.py
+++ case.py
@@ -1,3 +1,24 @@
+## -*- coding: utf-8 -*-
+##
+## case.py
+##
+## Author:   Toke Høiland-Jørgensen (toke@toke.dk)
+## Date:     26 April 2012
+## Copyright (c) 2012, Toke Høiland-Jørgensen
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from attributes import BaseAttribute
 import attribute_names
 
