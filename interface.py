e03740eadf0aad5c0556e803007db2ea8cb567c2
Toke Høiland-Jørgensen <thoe898@aucklanduni.ac.nz>
2012-04-26 20:38:51 +1200

    Add setting and unsetting of attributes for query. Only valid attributes so far.

--- interface.py
+++ interface.py
@@ -19,9 +19,15 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import re
+import re, inspect
 
 from console import Console
+from case import Case
+from table_printer import print_table
+import attribute_names
+
+# Possible attribute names are all classes defined in the attribute_names module
+possible_attributes = sorted([i[0] for i in inspect.getmembers(attribute_names, inspect.isclass)])
 
 class Interface(Console):
 
@@ -33,20 +39,63 @@ class Interface(Console):
         if not cases:
             self.intro += "\nNOTE: Currently no cases loaded (you may want to run parser.py to generate some)!"
 
+        self.query = Case()
+        self.result = []
+
     def gen_help(self, method):
         """Generate a help message by removing extra spaces from doc strings"""
         helpstring = getattr(self.__class__, method).__doc__
         return re.sub("\n *", "\n", helpstring)
+
+    def do_help(self, arg):
+        if arg in ('status', 'query', 'result'):
+            super(Interface, self).do_help(arg)
+        else:
+            print "\n".join(['These are the accepted commands.',
+                             'Type help <command> to get help on a specific command.',
+                             '',
+                             'status    Show summary of system status.',
+                             'query     Manipulate and run query.',
+                             'result    Show result of a query.',
+                             'config    Set config variables.'])
 
     def help_help(self):
         print self.gen_help("do_help"),
 
     def do_status(self, arg):
         """Print current status of system (i.e. how many cases loaded etc)."""
-        print "Current %d cases loaded." % len(self.cases)
+        print "Currently %d cases loaded." % len(self.cases)
+        if self.query:
+            print "Current query has %d properties." % len(self.query)
+        else:
+            print "No current query."
+        if self.result:
+            print "Result exists."
+        else:
+            print "No result exists."
 
     def help_status(self):
         print self.gen_help("do_status")
+
+    def do_query(self, arg):
+        if arg in ('', 'show'):
+            if self.query:
+                print_table([self.query], ["Attribute", "Value"])
+            else:
+                print "No current query."
+        elif arg == "reset":
+            self.query = Case()
+            print "Query reset."
+        elif arg.startswith('set'):
+            arg,key,val = arg.split()
+            self.query[key] = val
+            print "Set '%s' with value '%s' to query." % (key,val)
+        elif arg.startswith('unset'):
+            arg,key = arg.split()
+            del self.query[key]
+            print "Unset attribute '%s' from query." % key
+        else:
+            print "Unrecognised argument. Type 'help query' for help."
 
     def default(self, line):
         print "Invalid command. Type 'help' for a list of commands."
