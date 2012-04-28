ef7cd9e6a5f486e21ed4359702ce6ac4a03530b3
Toke Høiland-Jørgensen <thoe898@aucklanduni.ac.nz>
2012-04-28 16:59:37 +1200

    Remove default filename from parser.py.

--- parser.py
+++ parser.py
@@ -82,7 +82,8 @@ def parse_item(lines):
 if __name__ == "__main__":
     import sys
     if len(sys.argv) < 2:
-        filename = "travel-cases.csv"
+        print "Usage: %s <filename>." % sys.argv[0]
+        sys.exit(1)
     else:
         filename = sys.argv[1]
 
